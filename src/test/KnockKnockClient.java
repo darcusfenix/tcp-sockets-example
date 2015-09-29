8ce733884df50352931eeb053dfd561003e6e019
juan <juancvfenix@gmail.com>
2015-09-28 23:07:27 -0500

    updating lap to pc

---
+++ src/test/KnockKnockClient.java
@@ -0,0 +1,72 @@
+/*
+ * The MIT License
+ *
+ * Copyright 2015 darcusfenix.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package test;
+
+/**
+ *
+ * @author darcusfenix
+ */
+import java.io.*;
+import java.net.*;
+
+public class KnockKnockClient {
+    public static void main(String[] args) throws IOException {
+        
+       
+
+        String hostName = "localhost";
+        int portNumber = 4444;
+
+        try (
+            Socket kkSocket = new Socket(hostName, portNumber);
+            PrintWriter out = new PrintWriter(kkSocket.getOutputStream(), true);
+            BufferedReader in = new BufferedReader(
+                new InputStreamReader(kkSocket.getInputStream()));
+        ) {
+            BufferedReader stdIn =
+                new BufferedReader(new InputStreamReader(System.in));
+            String fromServer;
+            String fromUser;
+
+            while ((fromServer = in.readLine()) != null) {
+                System.out.println("Server: " + fromServer);
+                if (fromServer.equals("Bye."))
+                    break;
+                
+                fromUser = stdIn.readLine();
+                if (fromUser != null) {
+                    System.out.println("Client: " + fromUser);
+                    out.println(fromUser);
+                }
+            }
+        } catch (UnknownHostException e) {
+            System.err.println("Don't know about host " + hostName);
+            System.exit(1);
+        } catch (IOException e) {
+            System.err.println("Couldn't get I/O for the connection to " +
+                hostName);
+            System.exit(1);
+        }
+    }
+}
---
+++ src/test/KnockKnockProtocol.java
@@ -0,0 +1,91 @@
+/*
+ * The MIT License
+ *
+ * Copyright 2015 darcusfenix.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package test;
+
+/**
+ *
+ * @author darcusfenix
+ */
+import java.net.*;
+import java.io.*;
+
+public class KnockKnockProtocol {
+    private static final int WAITING = 0;
+    private static final int SENTKNOCKKNOCK = 1;
+    private static final int SENTCLUE = 2;
+    private static final int ANOTHER = 3;
+
+    private static final int NUMJOKES = 5;
+
+    private int state = WAITING;
+    private int currentJoke = 0;
+
+    private String[] clues = { "Turnip", "Little Old Lady", "Atch", "Who", "Who" };
+    private String[] answers = { "Turnip the heat, it's cold in here!",
+                                 "I didn't know you could yodel!",
+                                 "Bless you!",
+                                 "Is there an owl in here?",
+                                 "Is there an echo in here?" };
+
+    public String processInput(String theInput) {
+        String theOutput = null;
+
+        if (state == WAITING) {
+            theOutput = "Knock! Knock!";
+            state = SENTKNOCKKNOCK;
+        } else if (state == SENTKNOCKKNOCK) {
+            if (theInput.equalsIgnoreCase("Who's there?")) {
+                theOutput = clues[currentJoke];
+                state = SENTCLUE;
+            } else {
+                theOutput = "You're supposed to say \"Who's there?\"! " +
+			    "Try again. Knock! Knock!";
+            }
+        } else if (state == SENTCLUE) {
+            if (theInput.equalsIgnoreCase(clues[currentJoke] + " who?")) {
+                theOutput = answers[currentJoke] + " Want another? (y/n)";
+                state = ANOTHER;
+            } else {
+                theOutput = "You're supposed to say \"" + 
+			    clues[currentJoke] + 
+			    " who?\"" + 
+			    "! Try again. Knock! Knock!";
+                state = SENTKNOCKKNOCK;
+            }
+        } else if (state == ANOTHER) {
+            if (theInput.equalsIgnoreCase("y")) {
+                theOutput = "Knock! Knock!";
+                if (currentJoke == (NUMJOKES - 1))
+                    currentJoke = 0;
+                else
+                    currentJoke++;
+                state = SENTKNOCKKNOCK;
+            } else {
+                theOutput = "Bye.";
+                state = WAITING;
+            }
+        }
+        return theOutput;
+    }
+}
